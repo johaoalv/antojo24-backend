934ab380385647d7be0fc95cf3b79aee6915391a
johaoenoc <johaoalvarado24@gmail.com>
2025-03-09 00:51:30 -0500

    first commit security

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.route("/api/pedido", methods=["POST"])
