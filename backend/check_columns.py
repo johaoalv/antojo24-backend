67590033d1a07add6e85f494289486277aaa3923
johaoalv <129918127+johaoalv@users.noreply.github.com>
2026-03-03 18:10:04 -0500

    Delete check_columns.py

--- backend/check_columns.py
+++
@@ -1,4 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-from db import fetch_all
-print(fetch_all("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'pedidos'"))
